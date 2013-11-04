d1a4017967f52be6a58e22ccc0d38d6f15c584a0
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-11-04 16:52:56 +0900

    로그인 창 view 붙임

--- app/routes.php
+++ app/routes.php
@@ -20,8 +20,13 @@ Route::group(array('prefix'=>'admin'),function(){
      Route::resource('answers','AdminAnswersController');
 });
 
-Route::resource('answers','AnswersController');
-
 Route::resource('answers', 'AnswersController');
-
 Route::resource('news', 'NewsController');
+
+// Account
+Route::get('login', 'AccountController@getLogin');
+// Route::post('/login', 'AccountController@postLogin');
+// Route::get('/register', 'AccountController@getRegister');
+// Route::post('/register', 'AccountController@postRegister');
+// Route::get('/logout', 'AccountController@getLogout');
+// Route::controller('account', 'AccountController');
